a1bf355afa9afa40c5e971ad58a4cb08cbe99d44
Yu Guo <yu.guo@secbit.io>
2024-12-13 17:49:33 +0800

    feat(transcript): add absorb() and squeeze() methods

--- src/merlin/merlin_transcript.py
+++ src/merlin/merlin_transcript.py
@@ -29,3 +29,23 @@ class MerlinTranscript:
         new_transcript = MerlinTranscript(label)
         new_transcript.strobe = Strobe128(self.strobe.state.copy(), self.strobe.pos, self.strobe.pos_begin, self.strobe.cur_flags)
         return new_transcript
+
+    def absorb(self, label: bytes, obj) -> None:
+        if obj is None:
+            self.append_message(label, b"padding")
+        else:
+            self.append_message(label, str(obj).encode())
+
+    def squeeze(self, _class, label: bytes, length: int) -> bytes:
+        """
+        Squeeze a value from the transcript.
+
+        Args:
+            _class: The class of the value to squeeze.
+            label: The label of the value to squeeze.
+            length: The length of the value to squeeze.
+
+        Returns:
+            The value squeezed from the transcript.
+        """
+        return _class.from_bytes(self.challenge_bytes(label, length))
